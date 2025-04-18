0a00a962fde5296aae1c9eab6f4b9380ac10d17a
Bucci <kibuchikahuthu@gmail.com>
2025-04-18 22:51:30 +0300

    Added a doctor scheduler program.

---
+++ Real World Scenarions - Objects and Classes/Health/Doctor Scheduler/doctor_scheduling.py
@@ -0,0 +1,43 @@
+class Doctor:
+    """
+    Attributes = name, specialty, shift, availability
+    Methods = assign_shift(), change_availability(), info()
+    Operations = manage availability, status and schedules
+    """
+    # Step 1 - Initialize
+    def __init__(self, name, specialty, shift, availability):
+        self.name = name # the doctors name
+        self.specialty = specialty # e.g. "Cardiology"
+        self.shift = None # no shift has been assigned yet
+        self.available = False # starts out as not available
+
+    """
+    Step 2 - Give the doctor a shift and mark them as available
+    """
+    def assign_shift(self, shift):
+        if not self.available:
+            self.shift = shift # record the new shift
+            self.available = True # now they are available
+            print(f"{self.name} is available is assigned to the {self.shift} shift.")
+        else:
+            print(f"{self.name} is already on the {self.shift} shift therefore he is unavailable.")
+
+    """
+    Step 3 - Change a doctor's availability
+    """
+    def change_availability(self, available):
+        if available and not self.shift: # means the doctor is available and not assigned to a shift
+            print(f"⚠ {self.name} has no shift assigned; cannot become available.")
+            return
+        # otherwise, accept the change, this makes the doctor available
+        self.available = available
+
+        status = "available" if available else "unavailable"
+
+        print(f"{self.name} is currently {status}")
+
+        
+
+
+
+
